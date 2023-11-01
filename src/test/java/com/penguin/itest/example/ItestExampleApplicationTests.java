10ed10a5a83a812c1c64d37438add14fc457ccd1
Arun Merum <penguin@Aruns-MacBook-Air.local>
2023-11-01 09:35:44 +0100

    Removes the docker dependency for the Integration tests

--- src/test/java/com/penguin/itest/example/ItestExampleApplicationTests.java
+++ src/test/java/com/penguin/itest/example/ItestExampleApplicationTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -20,6 +22,12 @@ class ItestExampleApplicationTests {
             .withPassword("password")
             .withDatabaseName("db");
 
+    @DynamicPropertySource
+    static void configureTestContainersProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+    }
     @Autowired
     CustomerRepository customerRepository;
 
