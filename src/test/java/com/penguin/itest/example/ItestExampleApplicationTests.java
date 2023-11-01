af9d1154466947115daad644e12fe43d1281b33c
Arun Merum <penguin@Aruns-MacBook-Air.local>
2023-11-01 09:44:06 +0100

    Uses higherlevel config

--- src/test/java/com/penguin/itest/example/ItestExampleApplicationTests.java
+++ src/test/java/com/penguin/itest/example/ItestExampleApplicationTests.java
@@ -6,28 +6,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
 class ItestExampleApplicationTests {
 
-    @Container
-    public static PostgreSQLContainer container = new PostgreSQLContainer<>("postgres:14")
-            .withUsername("username")
-            .withPassword("password")
-            .withDatabaseName("db");
-
-    @DynamicPropertySource
-    static void configureTestContainersProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", container::getJdbcUrl);
-        registry.add("spring.datasource.username", container::getUsername);
-        registry.add("spring.datasource.password", container::getPassword);
-    }
     @Autowired
     CustomerRepository customerRepository;
 
